29ad04349494148a492370e11e52176fe13d0677
SerhiiOberemchuk <135756668+SerhiiOberemchuk@users.noreply.github.com>
2024-01-13 21:08:37 +0000

    Deploying to gh-pages from @ SerhiiOberemchuk/goit-react-hw-08-phonebook@111cf29a0332097396d958351ab45b182f1013c2 🚀

---
+++ static/js/415.0cca6993.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:(e,t,a)=>{a.r(t),a.d(t,{default:()=>c});a(791);var s=a(184);const c=()=>(0,s.jsxs)("div",{className:"container text-center",children:[(0,s.jsx)("h1",{className:" mt-5",children:"Welcome to the phonebook! "}),(0,s.jsx)("p",{className:"lead",children:"To access contacts, please follow the 'Contacts' page, 'logIn', or 'Registration'"})]})}}]);
+//# sourceMappingURL=415.0cca6993.chunk.js.map
